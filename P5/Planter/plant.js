045cafe99f28fef662cc4c366d506f55dac70c86
BingoMango <103103876+Dange1TheMange1@users.noreply.github.com>
2024-04-09 10:36:14 +0200

    Der er knapper nu, jeg hader hvordan jeg har lavet dem

--- P5/Planter/plant.js
+++ P5/Planter/plant.js
@@ -1,19 +1,15 @@
-// The Coding Train / Daniel Shiffman
-// WebGL - loadModel
-
-// Video: https://youtu.be/FUI7HEEz9B0
-
-let angle = 0;
 let plantA;
 let textureA;
+var displayNum;
 
 function preload() {
   plantA = loadModel('PlantA.obj', true);
   textureA = loadImage('Textureblomst.png');
+  displayNum = 0;
 }
 
 function setup() {
-  createCanvas(400, 300, WEBGL);
+  createCanvas(windowWidth, windowHeight-150, WEBGL);
 }
 
 function draw() {
@@ -25,9 +21,16 @@ function draw() {
   //calculations
   scaler = 5 * sin(frameCount * windowHeight*0.00002); //extremely magic number
 
-  //actions
-  rotateY(frameCount * 0.01);
   translate(0,scaler,0);
+
+  //actions (thsi code is very stupid, future works, this needs to scale better)
+  if(displayNum==1){
+  rotateY(frameCount * 0.01);
+  }else if(displayNum == 2){
+    rotateY(frameCount * -0.01);
+  }else if(displayNum == 3){
+    rotateY(frameCount * 0.01);
+  }
 
   //plant rendering
   texture(textureA);
---
+++ P5/Planter/wikiHandler.js
@@ -0,0 +1,13 @@
+let url = 'https://en.wikipedia.org/wiki/Triplofusus_giganteus';
+//den her kode er lort
+function displayA() {
+    displayNum = 1;
+}
+
+function displayB() {
+    displayNum = 2;
+}
+
+function displayC() {
+    displayNum = 3;
+}
