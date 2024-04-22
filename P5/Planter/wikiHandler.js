630d647b140ae35ceb47ebd392fe20d6f998cc61
BingoMango <103103876+Dange1TheMange1@users.noreply.github.com>
2024-04-22 09:36:39 +0200

    Man kan nu trykke på knap for at få sin location
    Det her betyder at API er checked.

--- P5/Planter/wikiHandler.js
+++ P5/Planter/wikiHandler.js
@@ -1,13 +1,37 @@
-let url = 'https://en.wikipedia.org/wiki/Triplofusus_giganteus';
+const myElement = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    myElement.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  myElement.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+} 
+
+
+
+//Crazy plant choosing
+let searchUrl = 'api.php?action=query&prop=pageimages&titles=Albert%20Einstein&pithumbsize=100';
+let plantName = "Plant";
+
 //den her kode er lort
 function displayA() {
     displayNum = 1;
+    plantName = "Christians uggerz plante" //det her skal tages fra JSON
 }
 
 function displayB() {
     displayNum = 2;
+    plantName = "Christian"
 }
 
 function displayC() {
     displayNum = 3;
+    plantName = "Deez"
 }
+
